58c47d9d4b9996b97c35e3d74058e3251fb41418
Vineeth2496 <vineetharmoori2496@gmail.com>
2023-04-21 12:22:23 +0530

    Java CUI & GUI

---
+++ MyFrame.java
@@ -0,0 +1,11 @@
+import java.awt.Frame;
+class MyFrame
+{
+	public static void main(String[]args)
+		{
+		Frame f=new Frame();
+		f.setTitle("Vineeth");
+		f.setSize(700,350);
+		f.setVisible(true);
+		}
+}
---
+++ MyFrame1.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+class MyFrame1
+{
+	public static void main(String[]args)
+		{
+		JFrame f=new JFrame();
+		f.setTitle("Vineeth");
+		f.setSize(700,350);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		f.setVisible(true);
+		}
+}
